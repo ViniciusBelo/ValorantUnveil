e79dd16df2a2f9fa72be884ade4f24775f3199ac
Gabriel Barbosa <gabriel.brbs84@gmail.com>
2024-06-05 19:18:08 -0300

    Enviando projeto com nginx

--- src/main/java/com/educandoweb/course/config/CorsConfig.java
+++ src/main/java/com/educandoweb/course/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 	    registry.addMapping("/users/**")
-	            .allowedOrigins("http://127.0.0.1:5500")
+	            .allowedOrigins("http://127.0.0.1:5500", "http://localhost")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE")
 	            .allowedHeaders("*");
 	}
